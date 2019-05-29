cbc12b9a9beb48b97fb14c692788e027cbd6517d
Jun_Hyeokwon <gogo4845_@naver.com>
2019-05-29 16:28:34 +0900

    Modularization

--- 3_flask/app.py
+++ 3_flask/app.py
@@ -1,8 +1,5 @@
-
 from flask import Flask, render_template  # pip install requests
-import requests  # pip install requests
-import random
-import json
+from . import lotto_package
 
 app = Flask(__name__)
 
@@ -14,69 +11,32 @@ def index():
 
 @app.route('/pick_lotto')
 def pick_lotto():
-    lucky_numbers = random.sample(range(1, 46), 6)
-    lucky_numbers.sort()
+    lucky_numbers = lotto_package.get_random_numbers()
     return render_template('pick_lotto.html', numbers=lucky_numbers)
 
 
 @app.route('/get_lotto/<int:num>')
 def get_lotto(num):
-    url = f'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={num}'
-    res = requests.get(url).text  # type == String
-    data = json.loads(res)  # type == dict
-
-    real_numbers = []
-
-    if data['returnValue'] == 'success':
-        for key, value in data.items():
-            if 'drwtNo' in key:
-                real_numbers.append(value)
-
-        real_numbers.sort()
+    data = lotto_package.get_lotto_numbers(num)
 
     return render_template(
         'get_lotto.html',
-        numbers=real_numbers,
+        numbers=data['real'],
         draw_no=num
     )
 
 
 @app.route('/lotto/<int:num>')
 def lotto(num):
-    lucky_numbers = random.sample(range(1, 46), 6)
-    lucky_numbers.sort()
+    lucky_numbers = lotto_package.get_random_numbers()
 
-    url = f'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={num}'
-    res = requests.get(url).text  # type == String
-    data = json.loads(res)  # type == dict
-    bonus_number = data['bnusNo']
+    real_data = lotto_package.get_lotto_numbers(num)  # { 'real': [1,2,3,4,5,6], 'bonus': 7 }
 
-    real_numbers = []
-
-    if data['returnValue'] == 'success':
-        for key, value in data.items():
-            if 'drwtNo' in key:
-                real_numbers.append(value)
-
-        real_numbers.sort()
+    real_numbers = real_data['real']
+    bonus_number = real_data['bonus']
 
     # 등수 비교
-    lucky = set(lucky_numbers)
-    real = set(real_numbers)
-
-    match_count = len(real.intersection(lucky))
-
-    result = '꽝'
-    if match_count == 6:
-        result = 1
-    elif match_count == 5 and bonus_number in lucky_numbers:
-        result = 2
-    elif match_count == 5:
-        result = 3
-    elif match_count == 4:
-        result = 4
-    elif match_count == 3:
-        result = 5
+    result = lotto_package.get_result(real_numbers, lucky_numbers, bonus_number)
 
     return render_template(
         'lotto.html',
